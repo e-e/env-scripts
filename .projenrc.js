d6a79c7e3a455938eeea1476aea9476b91876afd
Eric Ellingson <god@eric.wtf>
2022-08-27 10:59:23 +0900

    add the bin script

--- .projenrc.js
+++ .projenrc.js
@@ -11,10 +11,7 @@ const project = new javascript.NodeProject({
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   devDeps: ["prettier@^2.7.1"] /* Build dependencies for this module. */,
   // packageName: undefined,  /* The "name" in package.json. */
-  // bin: {
-  //   "env-scripts-diff": "bin/diff.js",
-  //   "env-scripts-sort": "bin/sort.js"
-  // }
+  bin: "lib/index.js",
   gitignore: [".idea"],
 });
 project.synth();
